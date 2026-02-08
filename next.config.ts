f4077ae10daf3051e1d76c07cb20cc16d7742692
Dev Singh <dev@devksingh.com>
2026-02-08 15:33:27 -0600

    Revert "Swap react for preact"
    This reverts commit 2b66dffa53ee7cb5697b2ca425e0859cde25783a.

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://static.acm.illinois.edu/**')],
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-
-    return config;
-  },
 };
 
 export default nextConfig;
